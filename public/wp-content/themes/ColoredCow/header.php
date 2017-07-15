9f942fed0e704492b36103e1369b4c7d433f4148
VamsiRao <vamsi@coloredcow.com>
2017-07-15 10:55:34 +0530

    #1 Create Navbar

--- public/wp-content/themes/ColoredCow/header.php
+++ public/wp-content/themes/ColoredCow/header.php
@@ -7,6 +7,23 @@
     <meta name="viewport" content="initial-scale=1">
     <title><?php echo bloginfo('name'); ?></title>
     <?php wp_head(); ?>
+
+	<nav class=" navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">				
+		<button class="navbar-toggler navbar-toggler-right justify-content-end" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/logo.png" width="180" height="45" class="d-inline-block align-top" alt=""></a>
+		<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+			<ul class="navbar-nav">
+				<li class="nav-item active">
+					<a role="button" class="btn btn-outline-secondary" href="guests_control_page.php" style="font-family: 'Oswald'; font-size: 17px;">Admin Login
+						<span class="sr-only">(current)</span>
+					</a>
+				</li>&nbsp;
+			</ul>
+		</div>
+	</nav>
+
 </head>
 
 <body>
