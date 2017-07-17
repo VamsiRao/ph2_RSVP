a78fb62f616a7856480bbb839069b3721ac80416
VamsiRao <vamsi@coloredcow.com>
2017-07-17 15:35:40 +0530

    Revise reviews 6

--- public/wp-content/themes/ColoredCow/header.php
+++ public/wp-content/themes/ColoredCow/header.php
@@ -3,10 +3,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1">
-    <title><?php echo bloginfo('name'); ?></title>
-    <?php wp_head(); ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1">
+	<title><?php echo bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
 
 	<nav class=" navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">				
 		<button class="navbar-toggler navbar-toggler-right justify-content-end" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,12 +14,12 @@
 		</button>
 		<a class="navbar-brand" href="#">
 			<?php $img_url=get_template_directory_uri().'/dist/img/logo.png'; ?>
-			<img src="<?= esc_url($img_url) ?>"  class="d-inline-block align-top" alt="">
+			<img src="<?php echo esc_url($img_url) ?>"  class="d-inline-block align-top" alt="coloredcow logo">
 		</a>
 		<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-					<a role="button" class="btn btn-outline-secondary font-oswald"  href="<?=admin_url()?>" >Admin Login
+					<a role="button" class="btn btn-outline-secondary font-oswald"  href="<?php echo admin_url()?>" >Admin Login
 						<span class="sr-only">(current)</span>
 					</a>
 				</li>
@@ -30,4 +30,4 @@
 </head>
 
 <body>
-    
+	
