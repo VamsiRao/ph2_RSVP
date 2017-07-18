756d8a138e6ffe180980401d9b20111a440bb91a
VamsiRao <vamsi@coloredcow.com>
2017-07-18 12:34:56 +0530

    Add request

--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -11,6 +11,11 @@
 		<div class="col-xs-12 col-md-6">
 			<h5 class="soiree-header font-marcellus">Soiree</h5>
 			<p class="soiree-text font-inherit" >ColoredCow celebrates every first Saturday of the month with family and friends. This custom has been started to take a little time off from work and enjoy some moments in life. we believe in sharing moments and learning with each other. Come and join us over music, food, drinks and some moments full of laughter and joy.</p>
+			<hr>				
+			<div class="col-md-odffset-1 col-md-12 col-xs-12 text-center">
+				<h5 class="join-header font-marcellus">Wanna Join The Party?</h5>				
+				<button type="button" class="btn btn-outline-warning btn-lg join-button">Request Invite</button>
+			</div>
 		</div>
 		<div class="col-xs-12 col-md-6">
 			
