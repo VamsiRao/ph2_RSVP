497946d9441a8ecf14bdf9b481d62481902c1213
VamsiRao <vamsi@coloredcow.com>
2017-07-18 11:33:45 +0530

    #2 Soiree heading and text

--- public/wp-content/themes/ColoredCow/landing.php
+++ public/wp-content/themes/ColoredCow/landing.php
@@ -6,7 +6,17 @@
 
     get_header();
 ?>
-
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-6">
+			<h5 class="soiree-header font-marcellus">Soiree</h5>
+			<p class="soiree-text font-inherit" >ColoredCow celebrates every first Saturday of the month with family and friends. This custom has been started to take a little time off from work and enjoy some moments in life. we believe in sharing moments and learning with each other. Come and join us over music, food, drinks and some moments full of laughter and joy.</p>
+		</div>
+		<div class="col-xs-12 col-md-6">
+			
+		</div>
+	</div>
+</div>
 
 
 <?php 
